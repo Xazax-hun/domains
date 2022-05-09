6d14fc535168273676c919eb2b54952adec3998c
Gabor Horvath <xazax.hun@gmail.com>
2022-05-08 20:57:35 -0700

    Add comments about the semantic requirements of some concepts.

--- include/dataflow/domains/domain.h
+++ include/dataflow/domains/domain.h
@@ -8,9 +8,19 @@
 template<typename T>
 concept Domain = requires(T a)
 {
+    // Required to be an equivalence relation.
     { a == a } -> std::convertible_to<bool>;
+    // Required to be partial order.
     { a <= a } -> std::convertible_to<bool>;
+    // Required to be the smallest element according to the order above.
     { T::bottom() } -> std::same_as<T>;
+    // Requirements:
+    // * a.merge(a) == a
+    // * a.merge(b) == b.merge(a)
+    // * a.merge(b) >= a
+    // * a.merge(b) >= b
+    // * top.merge(b) == top
+    // * bottom.merge(b) == b
     { a.merge(a) } -> std::same_as<T>;
     { a.toString() } -> std::convertible_to<std::string_view>;
 };
@@ -19,6 +29,10 @@ template<typename T>
 concept WidenableDomain = Domain<T> &&
     requires(T a)
 {
+    // Requirements:
+    // * bottom.widen(a) == a
+    // * a.widen(a) == a
+    // * b.widen(a) == b if a <= b
     { a.widen(a) } -> std::same_as<T>; 
 };
 
