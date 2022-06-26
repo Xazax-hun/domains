c2eaf2637f87b0e1354ae9557527660cba5c43e3
Gabor Horvath <xazax.hun@gmail.com>
2022-06-26 15:24:36 -0700

    Self loops are now considered back edges.

--- src/eval.cpp
+++ src/eval.cpp
@@ -125,8 +125,8 @@ Walk createRandomWalk(const CFG& cfg, int loopiness)
         }
         if (cfg.blocks()[current].successors().empty())
             break;
-        backEdges.merge(detectBackEdges(cfg, current, visited));
         visited.insert(current);
+        backEdges.merge(detectBackEdges(cfg, current, visited));
         current = getNextBlock(gen, cfg, current, backEdges, loopiness);
     } while (true);
 
