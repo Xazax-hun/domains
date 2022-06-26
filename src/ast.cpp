58fc7e4a9c67aa462694bc3e227da41a82a0a70c
Gabor Horvath <xazax.hun@gmail.com>
2022-06-25 18:59:14 -0700

    Simplify AST printing.

--- src/ast.cpp
+++ src/ast.cpp
@@ -12,31 +12,47 @@ namespace
         return std::string(n, ' ');
     }
 
+    template<bool Pre = false>
+    void renderAnnotations(std::ostream& out, Node n, const Annotations::AnnotationMap& map) noexcept
+    {
+        if (auto it = map.find(n); it != map.end())
+        {
+            if (!it->second.empty())
+            {
+                out << (Pre ? "" : " ") << "/*";
+                for (const auto& annotation : it->second)
+                    out << " " << annotation;
+
+                out << " */" << (Pre ? " " : "");
+            }
+        }
+    }
+
+    void renderPreAnnotations(std::ostream& out, Node n, const Annotations& anns) noexcept
+    {
+        renderAnnotations</*Pre =*/true>(out, n, anns.preAnnotations);
+    }
+
+    void renderPostAnnotations(std::ostream& out, Node n, const Annotations& anns) noexcept
+    {
+        renderAnnotations(out, n, anns.postAnnotations);
+    }
+
     struct NodePrinter {
         void operator()(const Init* i) const noexcept
         {
-            out << indentString(indent);
-            renderPreAnnotations(i);
             out << fmt::format("init({}, {}, {}, {})", *i->topX.value, *i->topY.value, *i->width.value, *i->height.value);
-            renderPostAnnotations(i);
         }
         void operator()(const Translation* t) const noexcept
         {
-            out << indentString(indent);
-            renderPreAnnotations(t);
             out << fmt::format("translation({}, {})", *t->x.value, *t->y.value);
-            renderPostAnnotations(t);
         }
         void operator()(const Rotation* r) const noexcept
         {
-            out << indentString(indent);
-            renderPreAnnotations(r);
             out << fmt::format("rotation({}, {}, {})", *r->x.value, *r->y.value, *r->deg.value);
-            renderPostAnnotations(r);
         }
         void operator()(const Sequence* s) const noexcept
         {
-            renderPreAnnotations(s);
             int i = 0;
             const int size = s->nodes.size();
             for (auto n : s->nodes)
@@ -45,52 +61,21 @@ namespace
                 if (i++ < size - 1)
                     out << ";\n";
             }
-            renderPostAnnotations(s);
         }
         void operator()(const Branch* b) const noexcept
         {
-            out << indentString(indent);
-            renderPreAnnotations(b);
             out << "{\n";
             print(indent + 2, b->lhs, out, anns);
             out << "\n" << indentString(indent) << "} or {\n";
             print(indent + 2, b->rhs, out, anns);
             out << "\n" << indentString(indent) << "}";
-            renderPostAnnotations(b);
         }
         void operator()(const Loop* l) const noexcept
         {
-            out << indentString(indent);
-            renderPreAnnotations(l);
             out << "iter {\n";
             print(indent + 2, l->body, out, anns);
             out << "\n" << indentString(indent) << "}";
-            renderPostAnnotations(l);
         }
-        void renderPreAnnotations(Node n) const noexcept
-        {
-            renderAnnotations</*Pre =*/true>(n, anns.preAnnotations);
-        }
-        void renderPostAnnotations(Node n) const noexcept
-        {
-            renderAnnotations(n, anns.postAnnotations);
-        }
-        template<bool Pre = false>
-        void renderAnnotations(Node n, const Annotations::AnnotationMap& map) const noexcept
-        {
-            if (auto it = map.find(n); it != map.end())
-            {
-                if (!it->second.empty())
-                {
-                    out << (Pre ? "" : " ") << "/*";
-                    for (const auto& annotation : it->second)
-                        out << " " << annotation;
-
-                    out << " */" << (Pre ? " " : "");
-                }
-            }
-        }
-
         int indent;
         std::ostream& out;
         const Annotations& anns;
@@ -107,8 +92,12 @@ std::string print(Node n, const Annotations& anns) noexcept
 
 void print(int indent, Node n, std::ostream& output, const Annotations& anns) noexcept
 {
+    if (!std::holds_alternative<const Sequence*>(n))
+        output << indentString(indent);
+    renderPreAnnotations(output, n, anns);
     NodePrinter nodePrinter{indent, output, anns};
     std::visit(nodePrinter, n);
+    renderPostAnnotations(output, n, anns);
 }
 
 
